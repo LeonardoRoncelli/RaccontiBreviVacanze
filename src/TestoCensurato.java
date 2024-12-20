cb1bf84c74d58e4e213b77ce03091f8ad6a7204d
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2024-12-20 11:52:09 +0100

    Scrittura della classe TestoCensurato.

--- src/TestoCensurato.java
+++ src/TestoCensurato.java
@@ -1,5 +1,17 @@
-public class TestoCensurato {
-    public static void main(String[]args){
-
+public class TestoCensurato extends Testo{
+    public String parolaProibita;
+    public void setParolaProibita(String parolaProibita){
+        this.parolaProibita=parolaProibita;
     }
+    public String getParolaProibita(){
+        return parolaProibita;
+    }
+    public TestoCensurato(String autore,String titolo,String contenuto,String parolaProibita){
+        super(autore, titolo, contenuto);
+        this.parolaProibita=parolaProibita;
+    }
+    public String toString(){
+        return "La parola proibita è: "+parolaProibita;
+    }
+
 }
