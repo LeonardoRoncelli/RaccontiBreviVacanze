5802c6adeec2910f6af30645d8cdee30be2d1756
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2024-12-20 11:48:53 +0100

    Scrittura della classe Testo.

--- src/Testo.java
+++ src/Testo.java
@@ -1,3 +1,33 @@
 public class Testo {
-
+    private String autore;
+    private String titolo;
+    private String contenuto;
+    public void setAutore(String autore){
+        this.autore=autore;
+    }
+    public String getAutore(){
+        return autore;
+    }
+    public void setTitolo(String titolo){
+        this.titolo=titolo;
+    }
+    public String getTitolo(){
+        return titolo;
+    }
+    public void setContenuto(String contenuto){
+        this.contenuto=contenuto;
+    }
+    public String getContenuto(){
+        return contenuto;
+    }
+    public Testo(String autore,String titolo,String contenuto){
+        this.autore=autore;
+        this.titolo=titolo;
+        this.contenuto=contenuto;
+    }
+    public String toString(){
+        return "Titolo del testo: "+titolo+"\n"+
+                "Autore del testo: "+autore+"\n"+
+                "Contenuto del testo: "+contenuto;
+    }
 }
---
+++ src/TestoCensurato.java
@@ -0,0 +1,5 @@
+public class TestoCensurato {
+    public static void main(String[]args){
+
+    }
+}
