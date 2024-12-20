dbf1dd647086f2d5af13fed191bbac4c02529a59
Roncelli <leonardoroncelli@gmail.com>
2024-12-20 18:24:14 +0100

    Aggiornamento main.

--- src/TestTesto.java
+++ src/TestTesto.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class TestTesto {
     public static void main(String[]args){
         Scanner input=new Scanner(System.in);
-        Testo testo=new Testo("Giuseppe Cruciani","Via Crux","Battaglia contro il politicamente corretto");
+        Testo testo=new Testo("Giuseppe Cruciani","Via Crux","Battaglia contro il politicamente corretto"); //CAMBIARE IL TITOLO POSSIBILMENTE
+        TestoCensurato censura=new TestoCensurato("Giuseppe Cruciani","Via Crux","Battaglia contro il politicamente corretto","battaglia");
+        String controlloContenuto=testo.getContenuto();
     }
 }
